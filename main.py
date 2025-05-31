8b1cb76b2c2a65ee835beb69aa859b653f429df8
addydr <112191393+addy-dr@users.noreply.github.com>
2025-05-31 16:30:29 +0100

    4b, 4c done

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def negative_log_likelihood(params, x_data, y_data):
     return nll
 
 
-def chi2_estimate(A_values, lamb_values, xs, ys):
+def chi2_estimate(A_values, lamb_values, xs, ys, mu=0, sig=1, signal_amp=0, output=True):
     '''
     Performs a χ² search for the best values of A and lambda.
     '''
@@ -38,20 +38,22 @@ def chi2_estimate(A_values, lamb_values, xs, ys):
     for i, A_trial in enumerate(A_values):
         for j, lamb_trial in enumerate(lamb_values):
             # Calculate expected background
-            B_expected = STOM_higgs_tools.get_B_expectation(xs, A_trial, lamb_trial)
+            B_expected = STOM_higgs_tools.get_SB_expectation(xs, A_trial, lamb_trial, mu, sig, signal_amp)
             chi2 = np.sum((ys - B_expected)**2 / B_expected)
             chi2_grid[i, j] = chi2
 
     min_index = np.unravel_index(np.argmin(chi2_grid), chi2_grid.shape)
     A_estimate, lamb_estimate = A_values[min_index[0]], lamb_values[min_index[1]]
     min_chi2 = chi2_grid[min_index]
-    print(f"χ² Results: A = {A_estimate:.1f}, λ = {lamb_estimate:.1f}, χ²/DoF = {min_chi2:.2f}")
+    if output:
+        print(f"χ² Results: A = {A_estimate:.1f}, λ = {lamb_estimate:.1f}, χ²/DoF = {min_chi2:.2f}")
     return A_estimate, lamb_estimate, min_chi2
 
 
 def main():
-    "The main data analysis"
-
+    '''
+    The main data analysis
+    '''
     # Generate data
     vals = np.array(STOM_higgs_tools.generate_data(n_signals=400))
 
@@ -98,19 +100,28 @@ def main():
     print(f"With signal (Background Only): p_value = {p_value} << 5%, should be rejected.")
 
     # signal plus background hypothesis
-    x = np.linspace(104, 155, 500)
-    y_gaussian = STOM_higgs_tools.get_SB_expectation(x, A_mle, lamb_mle, GAUSS_MEAN, GAUSS_STD, GAUSS_AMP)
+    ff_A_chi2, ff_lamb_chi2, ff_chi2_min = chi2_estimate(A_values, lamb_values, mean, bin_height, GAUSS_MEAN, GAUSS_STD, GAUSS_AMP)
+    p_value = 1 - sp_chi2.cdf(ff_chi2_min, (len(mean) - 2))
+    print(f"\nFull fit with signal and background: p_value = {p_value} < 5%, do not reject.")
+
+    # find amplitude for 5% p value
+    for amp in range(1100,1200): #trial and error
+        __A_chi2, __lamb_chi2, __chi2_min = chi2_estimate(A_values, lamb_values, mean, bin_height, GAUSS_MEAN, GAUSS_STD, amp, False)
+        p_value = 1 - sp_chi2.cdf(__chi2_min, (len(mean) - 2))
+        if round(p_value,3) == 0.05:
+            break
+    print(f"\nFor a p-val of 5.0%, we require a signal amplitude of {amp}")
 
 
-
-    # Plot both fits
+    # Plot fits
     plt.rcParams.update({'font.size': 12, 'font.family': 'serif', 'figure.dpi': 400})
     fig, ax = plt.subplots(figsize=(6, 4))
 
     x = np.linspace(104, 155, 500)
     ax.plot(x, STOM_higgs_tools.get_B_expectation(x, A_mle, lamb_mle), label="MLE Fit", color='red')
     ax.plot(x, STOM_higgs_tools.get_B_expectation(x, A_chi2, lamb_chi2), '--', label="χ² Fit (background)", color='blue')
-    ax.plot(x, STOM_higgs_tools.get_B_expectation(x, A_chi2_with_signal, lamb_chi2_with_signal), '--', label="χ² Fit (background + signal)", color='yellow')
+    #ax.plot(x, STOM_higgs_tools.get_B_expectation(x, A_chi2_with_signal, lamb_chi2_with_signal), '--', label="χ² Fit (background + signal)", color='yellow')
+    ax.plot(x, STOM_higgs_tools.get_SB_expectation(x, ff_A_chi2, ff_lamb_chi2, GAUSS_MEAN, GAUSS_STD, GAUSS_AMP), '--', label="χ² Fit (background + signal)", color='green')
     ax.errorbar(mean, bin_height, yerr=ystd, xerr=xstd, fmt='o', markersize=3, color='black')
 
     ax.set_xlim(104, 155)
